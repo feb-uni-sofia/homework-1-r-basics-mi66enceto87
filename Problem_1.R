49921696db55ac1cd5965a9cb27a25939ef7ee3c
mi66enceto87 <37153689+mi66enceto87@users.noreply.github.com>
2018-03-28 10:37:37 +0300

    Михаела Ангелова
    ФNo 30465

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,21 @@
+# a)
+x <- c(4, 1, 1, 4)
+
+# b)
+y <- c(1, 4)
+
+# c)
+# This code relies on the order of the vector
+x-y
+
+# d)
+s <- c(x, y)
+
+# e)
+rep(s, 10)
+
+length (rep(s, 10))
+
+# f)
+rep(s, each = 3)
 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,48 @@
+# a)
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+
+# b)
+xmax-xmin
+
+# c)
+avgxmin <- mean(xmin)
+avgxmax <- mean(xmax)
+
+# d)
+xmin [xmin<mean(xmin)]
+
+# e)
+xmax [xmax>mean(xmax)]
+
+# f)
+names(xmin) <- xmin
+names(xmax) <- xmax
+
+# g)
+temperatures <- data.frame(xmin = xmin, xmax = xmax)
+temperatures
+
+# h)
+temperatures <- within (temperatures, {
+  xminFahrenheit <- (9/5)*xmin + 32
+  xmaxFahrenheit <- (9/5)*xmax + 32
+}
+)
+temperatures
+
+# i)
+temperatures <- within (temperatures, {
+}
+)
+xmax*9/5 + 32
+
+temperatures
+
+
+# j)
+temperatures[-(6:7), c('xminFahrenheit', 'xmaxFahrenheit')]
+
+
+
 
